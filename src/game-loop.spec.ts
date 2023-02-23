769b580b8839600290a2e11009fc9468b256517c
pb-uk <23248021+pb-uk@users.noreply.github.com>
2023-02-23 16:43:15 +0000

    Use JSDOM instead of bespoke mocks

--- src/game-loop.spec.ts
+++ src/game-loop.spec.ts
@@ -1,49 +1,18 @@
 import { expect } from 'chai';
 import * as sinon from 'sinon';
+import { JSDOM } from 'jsdom';
 
 import * as GameLoop from './game-loop.js';
 
-const mockVisibility = () => {
-  let visibilityChangeCallback: unknown;
-
-  global.document = global.document || {};
-  global.document.addEventListener = (event: string, callback: unknown) => {
-    visibilityChangeCallback = callback;
-  };
-
-  return ({
-    triggerVisibilityChange(state: string) {
-      if (visibilityChangeCallback == null) {
-        throw new Error('Callback not set');
-      }
-      document.visibilityState = state;
-      visibilityChangeCallback();
-    },
-  });
-};
-
-const mockAnimationFrame = () => {
-  let rafCallback: FrameRequestCallback|null;
-
-  global.window = global.window || {};
-  global.window.cancelAnimationFrame = sinon.fake(),
-  global.window.requestAnimationFrame = (callback) => {
-    rafCallback = callback;
-    return 1;
-  };
-
-  return ({
-    triggerAnimationFrame() {
-      if (rafCallback == null) {
-        throw new Error('Callback not set');
-      }
-      rafCallback(performance.now());
-      rafCallback = null;
-    },
-  });
-};
-
 describe('GameLoop', function () {
+
+  beforeEach(function () {
+    // eslint-disable-next-line
+    // @ts-ignore
+    global.window = new JSDOM('', { pretendToBeVisual: true }).window;
+    global.document = global.window.document; // new JSDOM().document;
+  });
+
   it('should export the gameState constants', function () {
     expect(GameLoop.HIDDEN).to.equal('hidden');
     expect(GameLoop.NOT_STARTED).to.equal('notStarted');
@@ -52,26 +21,30 @@ describe('GameLoop', function () {
   });
 
   it('should create and run an empty game', async function () {
-    const af = mockAnimationFrame();
     const game = GameLoop.createGame();
 
-    af.triggerAnimationFrame();
+    // af.triggerAnimationFrame();
 
     expect(game.start).not.to.throw;
     expect(game.stop).not.to.throw;
   });
 
   it('should call the update and render functions', function () {
-    const af = mockAnimationFrame();
     const update = sinon.fake();
     const render = sinon.fake();
+    const spy = sinon.spy(window, 'requestAnimationFrame');
     const game = GameLoop.createGame({ update, render });
+
     game.start();
 
+    // Should not be called until the next animation frame.
     expect(update.called).to.be.false;
     expect(render.called).to.be.false;
 
-    af.triggerAnimationFrame();
+    // Trigger the animation frame callback.
+    spy.getCall(0).args[0](performance.now());
+    // Need to stop otherwise it will hang waiting for the next animation frame.
+    game.stop();
 
     expect(update.called).to.be.true;
     expect(render.called).to.be.true;
